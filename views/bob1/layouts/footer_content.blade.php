e680381cfc783d45154370dd01429781aae7f147
Bob Bloom <info@southlasalle.com>
2016-02-22 18:22:32 -0500

    Minor modifications to the admin footer. #23

--- views/bob1/layouts/footer_content.blade.php
+++ views/bob1/layouts/footer_content.blade.php
@@ -5,6 +5,8 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
         <b>Version</b> {{{ $version->version() }}}
+        <br />
+        <a href="{{{ route('admin.installedpackages') }}}"><i class="fa fa-book fa-fw"></i><span>List of LaSalle Software installed packages</span></a><
     </div>
     <strong>You are using the {{{ $version->packageName() }}}.<br />Crafted with love, with the <a href="http://laravel.com" target="_blank">Laravel Framework v5.1</a>. Copyright © 2015 - 2016
 </footer>
